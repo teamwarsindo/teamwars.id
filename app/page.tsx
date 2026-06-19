21bdb3ad9cd34c03e5f353608e49c28c6810cce4
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-19 07:00:11 +0700

    Update button styles in page component

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Page() {
               href="/rules"
               className={cn(
                 buttonVariants({ size: "lg", variant: "outline" }),
-                "h-11 w-full gap-2 bg-gray-200 text-black hover:bg-gray-300 dark:bg-white dark:text-black dark:hover:bg-gray-200 lg:h-12 lg:w-auto lg:gap-2.5 lg:text-base [&_svg:not([class*='size-'])]:size-4 lg:[&_svg:not([class*='size-'])]:size-5",
+                "h-11 w-full gap-2 bg-gray-800 text-white hover:bg-gray-900 dark:bg-[#5865F2] dark:shadow-[0_0_30px_-6px_#5865F2] dark:hover:bg-[#4752c4] lg:h-12 lg:w-auto lg:gap-2.5 lg:text-base [&_svg:not([class*='size-'])]:size-4 lg:[&_svg:not([class*='size-'])]:size-5",
                 // "h-11 w-full gap-2 border-border/50 bg-transparent px-6 text-sm font-semibold text-foreground hover:bg-accent hover:text-accent-foreground lg:h-12 lg:w-auto lg:gap-2.5 lg:text-base [&_svg:not([class*='size-'])]:size-4 lg:[&_svg:not([class*='size-'])]:size-5",
               )}
             >
