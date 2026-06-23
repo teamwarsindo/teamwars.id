3f53cf640925fe7d38e4d6161391ba8f5ae4a6b3
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-23 23:25:45 +0700

    Remove redirects from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/invite',
-        destination: 'https://discord.gg/NtBBdqUrxe',
-        // Gunakan permanent: false agar jika suatu saat link Discord-mu expired dan kamu ganti link baru, browser pengunjung tidak menyimpan cache link yang lama.
-        permanent: false, 
-      },
-    ]
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
