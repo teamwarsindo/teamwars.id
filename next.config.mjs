63233fde841354d3a729a06550a12933ff4eb5c3
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-23 22:30:50 +0700

    Delete redirect for '/rules' to Google Docs
    Removed redirect rule for Google Docs link.

--- next.config.mjs
+++ next.config.mjs
@@ -8,11 +8,6 @@ const nextConfig = {
         // Gunakan permanent: false agar jika suatu saat link Discord-mu expired dan kamu ganti link baru, browser pengunjung tidak menyimpan cache link yang lama.
         permanent: false, 
       },
-      {
-        source: '/rules',
-        destination: 'https://docs.google.com/document/d/12nvtsG84Us674fte5joAPLY50wlwxHhJhSLew6wn0cw/edit?usp=drivesdk',
-        permanent: false,
-      },
     ]
   },
 };
