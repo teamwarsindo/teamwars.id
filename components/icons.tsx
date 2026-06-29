238aa8e27f68d75d266dfa18f6d860c95a8cc93b
Team Wars Indonesia <teamwars.indo@gmail.com>
2026-06-30 00:06:20 +0700

    Add LoaderIcon component to icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -295,3 +295,33 @@ export function ChevronUpIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function LoaderIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <circle 
+        className="opacity-25" 
+        cx="12" 
+        cy="12" 
+        r="10" 
+        stroke="currentColor" 
+        strokeWidth="4"
+      />
+      <path 
+        className="opacity-75" 
+        fill="currentColor" 
+        stroke="none"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" 
+      />
+    </svg>
+  )
+}
